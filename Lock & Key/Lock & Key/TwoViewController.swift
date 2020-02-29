cb97716f54e6123a2e033569bb92f22592444a68
Robert Keffury <bobby.keffury@me.com>
2020-02-28 20:47:52 -0800

    Added Outlets for Level 2 Storyboard.

--- Lock & Key/Lock & Key/TwoViewController.swift
+++ Lock & Key/Lock & Key/TwoViewController.swift
@@ -14,6 +14,16 @@ class TwoViewController: UIViewController {
     
     //MARK: - Outlets
     
+    @IBOutlet weak var level2Button: UIButton!
+    @IBOutlet weak var topMiddleSwipeView: UIView!
+    @IBOutlet weak var centerMiddleSwipeView: UIView!
+    @IBOutlet weak var middleLeftSwipeView: UIView!
+    @IBOutlet weak var middleRightSwipeView: UIView!
+    @IBOutlet weak var bottomMiddleSwipeView: UIView!
+    
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var keysButton: UIButton!
+    
     //MARK: - Views
     
     override func viewDidLoad() {
@@ -23,6 +33,12 @@ class TwoViewController: UIViewController {
     //MARK: - Methods
     
     //MARK: - Actions
+    
+    @IBAction func homeButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func level2ButtonTapped(_ sender: Any) {
+    }
     
     //MARK: - Navigation
 
