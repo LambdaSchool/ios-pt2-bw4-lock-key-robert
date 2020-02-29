7236614339e5f2a295407e8591cdb59d4643c57b
Robert Keffury <bobby.keffury@me.com>
2020-02-28 21:58:10 -0800

    Added Animation Methods.

--- Lock & Key/Lock & Key/SharedController.swift
+++ Lock & Key/Lock & Key/SharedController.swift
@@ -8,3 +8,45 @@
 
 import Foundation
 import UIKit
+
+class SharedController {
+    
+    //MARK: - Animation Methods
+    
+    func rotateKeysLeft(for button: UIButton) {
+        UIView.animate(withDuration: 4.0, animations: {
+            button.transform = CGAffineTransform(rotationAngle: -.pi/2)
+        })
+    }
+    
+    func rotateKeysDown(for button: UIButton) {
+        UIView.animate(withDuration: 4.0, animations: {
+            button.transform = CGAffineTransform(rotationAngle: -.pi)
+        })
+    }
+    
+    func rotateKeysRight(for button: UIButton) {
+        UIView.animate(withDuration: 4.0, animations: {
+            button.transform = CGAffineTransform(rotationAngle: .pi/2)
+        })
+    }
+    
+    func rotateKeysUp(for button: UIButton) {
+        UIView.animate(withDuration: 4.0, animations: {
+            button.transform = CGAffineTransform(rotationAngle: 0)
+        })
+    }
+    
+    func fadeKeysIn(for button: UIButton) {
+        UIView.animate(withDuration: 4.0, animations: {
+            button.alpha = 1.0
+        })
+    }
+    
+    //MARK: - Riddle Methods
+    
+    
+    
+    
+    
+}
