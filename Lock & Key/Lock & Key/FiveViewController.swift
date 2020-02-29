c87ebed7b648e517d15002b587822b14f369671e
Robert Keffury <bobby.keffury@me.com>
2020-02-28 21:06:02 -0800

    Added Outlets for Level 5 Storyboard.

--- Lock & Key/Lock & Key/FiveViewController.swift
+++ Lock & Key/Lock & Key/FiveViewController.swift
@@ -14,6 +14,21 @@ class FiveViewController: UIViewController {
     
     //MARK: - Outlets
     
+    @IBOutlet weak var level5Button: UIButton!
+    @IBOutlet weak var keysButton: UIButton!
+    
+    @IBOutlet weak var bottomSwipeView: UIView!
+    @IBOutlet weak var topSwipeView: UIView!
+    @IBOutlet weak var middleSwipeView: UIView!
+    
+    @IBOutlet weak var topLeftHoldView: UIView!
+    @IBOutlet weak var topRightHoldView: UIView!
+    @IBOutlet weak var bottomRightHoldView: UIView!
+    @IBOutlet weak var bottomLeftHoldView: UIView!
+    
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
     //MARK: - Views
     
     override func viewDidLoad() {
@@ -23,6 +38,14 @@ class FiveViewController: UIViewController {
     //MARK: - Methods
     
     //MARK: - Actions
+    
+    @IBAction func homeButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func level5ButtonTapped(_ sender: Any) {
+        
+    }
     
     //MARK: - Navigation
 
