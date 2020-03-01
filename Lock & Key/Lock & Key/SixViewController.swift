2cf5690a02f2ab9f974e10a5ccb9a93d5e5b490c
Robert Keffury <bobby.keffury@me.com>
2020-03-01 12:21:49 -0800

    Level 6 VC File Complete.

--- Lock & Key/Lock & Key/SixViewController.swift
+++ Lock & Key/Lock & Key/SixViewController.swift
@@ -12,6 +12,8 @@ class SixViewController: UIViewController {
 
     //MARK: - Properties
     
+    let sharedController = SharedController()
+    
     //MARK: - Outlets
     
     @IBOutlet weak var level6Button: UIButton!
@@ -25,29 +27,94 @@ class SixViewController: UIViewController {
     
     @IBOutlet weak var topHoldView: UIView!
     
+    @IBOutlet var holdRecognizer: UILongPressGestureRecognizer!
+    @IBOutlet var middleLeftSwipeDownRecognizer: UISwipeGestureRecognizer!
+    @IBOutlet var middleRightSwipeUpRecognizer: UISwipeGestureRecognizer!
+    
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         UIApplication.shared.isIdleTimerDisabled = true
+        self.view.alpha = 0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        sharedController.fadeViewIn(view: self.view)
+        bordersOn()
     }
     
     //MARK: - Methods
     
+    private func bordersOn() {
+        sharedController.setupButtonBorders(for: bottomLeftButton)
+        sharedController.setupButtonBorders(for: bottomRightButton)
+        
+        sharedController.setupSwipeViewBorders(for: middleLeftSwipeView)
+        sharedController.setupSwipeViewBorders(for: middleRightSwipeView)
+        
+        sharedController.setUpHoldViewBorders(for: topHoldView)
+    }
+    
     //MARK: - Actions
     
-    @IBAction func homeButtonTapped(_ sender: Any) {
-        
-    }
-    
     @IBAction func level6ButtonTapped(_ sender: Any) {
+        sharedController.addRiddleAlert(riddle: "", answer: "", clue: "", viewController: self, button: bottomLeftButton, gesture: nil, segue: "openingSegue")
+    }
+
+    @IBAction func bottomLeftButtonTapped(_ sender: Any) {
+        sharedController.fadeKeysIn(for: keysButton)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "", viewController: self, button: self.bottomRightButton, gesture: nil, segue: "openingSegue")
+        })
+    }
+    
+    @IBAction func bottomRightButtonTapped(_ sender: Any) {
+        sharedController.rotateKeysLeft(for: keysButton)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "", viewController: self, button: nil, gesture: self.middleLeftSwipeDownRecognizer, segue: "openingSegue")
+        })
+    }
+    
+    @IBAction func middleLeftSwipeDownComplete(_ sender: Any) {
+        if middleLeftSwipeDownRecognizer.state == .ended {
+            sharedController.rotateKeysDown(for: keysButton)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "", viewController: self, button: nil, gesture: self.middleRightSwipeUpRecognizer, segue: "openingSegue")
+            })
+        }
+    }
+    
+    @IBAction func middleRightSwipeUpComplete(_ sender: Any) {
+        if middleRightSwipeUpRecognizer.state == .ended {
+            sharedController.rotateKeysRight(for: keysButton)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "", viewController: self, button: nil, gesture: self.holdRecognizer, segue: "openingSegue")
+            })
+        }
         
     }
     
-    //MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBAction func holdComplete(_ sender: Any) {
+        if holdRecognizer.state == .ended {
+            sharedController.rotateKeysUp(for: keysButton)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.sharedController.addTransitionRiddleAlert(riddle: "", answer: "", viewController: self, segue: "7Segue", level: "6", homeSegue: "openingSegue")
+            })
+        }
     }
-
+    
+    @IBAction func homeButtonTapped(_ sender: Any) {
+        sharedController.fadeViewOut(view: self.view)
+        
+        sharedController.segueAfterFadeOut(viewController: self, segue: "openingSegue")
+    }
 }
