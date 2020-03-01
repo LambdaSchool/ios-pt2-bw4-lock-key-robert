f75b0a6b9215db6f9c81ae6e358799d4f85965ff
Robert Keffury <bobby.keffury@me.com>
2020-03-01 14:00:16 -0800

    Finish View Controller Finished.

--- Lock & Key/Lock & Key/FinishViewController.swift
+++ Lock & Key/Lock & Key/FinishViewController.swift
@@ -12,6 +12,9 @@ class FinishViewController: UIViewController {
 
     //MARK: - Properties
     
+    private let firework = Firework()
+    let sharedController = SharedController()
+    
     //MARK: - Outlets
     
     @IBOutlet weak var congratulationsLabel: UILabel!
@@ -20,15 +23,41 @@ class FinishViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.alpha = 0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        sharedController.fadeViewIn(view: self.view)
+        giddyUp()
     }
     
     //MARK: - Methods
     
-    //MARK: - Actions
-    
-    //MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    private func setOffFirework() {
+        firework.particleImage = UIImage(named: "circle")
+        firework.flareImage = UIImage(named: "circle")
+        firework.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(firework)
+        
+        NSLayoutConstraint.activate([
+            firework.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            firework.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            firework.topAnchor.constraint(equalTo: view.topAnchor),
+            firework.bottomAnchor.constraint(equalTo: congratulationsLabel.centerYAnchor)])
     }
-
+    
+    private func giddyUp() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            self.setOffFirework()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 7.0, execute: {
+                self.sharedController.fadeLabelInThenOut(label: self.congratulationsLabel, delay: 1.0)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 4.0, execute: {
+                    self.performSegue(withIdentifier: "openingSegue", sender: self)
+                })
+            })
+        })
+    }
 }
