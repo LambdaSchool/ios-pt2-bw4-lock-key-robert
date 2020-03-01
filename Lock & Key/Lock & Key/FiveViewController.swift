cf0956c4870f5839772c04ba54163b16f7f0d644
Robert Keffury <bobby.keffury@me.com>
2020-03-01 11:10:42 -0800

    Added Outlets, Actions & View Methods.

--- Lock & Key/Lock & Key/FiveViewController.swift
+++ Lock & Key/Lock & Key/FiveViewController.swift
@@ -12,6 +12,8 @@ class FiveViewController: UIViewController {
 
     //MARK: - Properties
     
+    let sharedController = SharedController()
+    
     //MARK: - Outlets
     
     @IBOutlet weak var level5Button: UIButton!
@@ -29,29 +31,73 @@ class FiveViewController: UIViewController {
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     
+    @IBOutlet var topLeftHoldRecognizer: UILongPressGestureRecognizer!
+    @IBOutlet var topRightHoldRecognizer: UILongPressGestureRecognizer!
+    @IBOutlet var bottomRightHoldRecognizer: UILongPressGestureRecognizer!
+    @IBOutlet var bottomLeftHoldRecognizer: UILongPressGestureRecognizer!
+    @IBOutlet var middleSwipeRecognizer: UISwipeGestureRecognizer!
+    
     //MARK: - Views
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         UIApplication.shared.isIdleTimerDisabled = true
+        self.view.alpha = 0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        sharedController.fadeViewIn(view: self.view)
+        bordersOn()
     }
     
     //MARK: - Methods
     
-    //MARK: - Actions
-    
-    @IBAction func homeButtonTapped(_ sender: Any) {
+    private func bordersOn() {
+        sharedController.setupSwipeViewBorders(for: topSwipeView)
+        sharedController.setupSwipeViewBorders(for: middleSwipeView)
+        sharedController.setupSwipeViewBorders(for: bottomSwipeView)
         
+        sharedController.setUpHoldViewBorders(for: topLeftHoldView)
+        sharedController.setUpHoldViewBorders(for: topRightHoldView)
+        sharedController.setUpHoldViewBorders(for: bottomRightHoldView)
+        sharedController.setUpHoldViewBorders(for: bottomLeftHoldView)
+        
+        sharedController.setupButtonBorders(for: leftButton)
+        sharedController.setupButtonBorders(for: rightButton)
     }
+    
+    //MARK: - Actions
     
     @IBAction func level5ButtonTapped(_ sender: Any) {
         
     }
     
-    //MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    @IBAction func topLeftHoldComplete(_ sender: Any) {
+        
     }
-
+    
+    @IBAction func topRightHoldComplete(_ sender: Any) {
+        
+    }
+    
+    @IBAction func bottomRightHoldComplete(_ sender: Any) {
+        
+    }
+    
+    @IBAction func bottomLeftHoldComplete(_ sender: Any) {
+        
+    }
+    
+    @IBAction func middleSwipeComplete(_ sender: Any) {
+        
+    }
+    
+    @IBAction func homeButtonTapped(_ sender: Any) {
+        sharedController.fadeViewOut(view: self.view)
+        
+        sharedController.segueAfterFadeOut(viewController: self, segue: "openingSegue")
+    }
 }
