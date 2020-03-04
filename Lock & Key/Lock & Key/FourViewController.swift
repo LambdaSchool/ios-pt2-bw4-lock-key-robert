670b8569e2ac15593233e2d7a92bc2f0d6ec3b05
Robert Keffury <bobby.keffury@me.com>
2020-03-04 10:43:12 -0800

    Added Answers for Level 4.

--- Lock & Key/Lock & Key/FourViewController.swift
+++ Lock & Key/Lock & Key/FourViewController.swift
@@ -61,7 +61,7 @@ class FourViewController: UIViewController {
     //MARK: - Actions
     
     @IBAction func level4ButtonTapped(_ sender: Any) {
-        sharedController.addRiddleAlert(riddle: "", answer: "", clue: "Tap Square", viewController: self, button: topLeftButton, gesture: nil, view: nil, segue: "openingSegue")
+        sharedController.addRiddleAlert(riddle: "", answer: "surfing", clue: "Tap Square", viewController: self, button: topLeftButton, gesture: nil, view: nil, segue: "openingSegue")
     }
     
     @IBAction func topLeftButtonTapped(_ sender: Any) {
@@ -69,7 +69,7 @@ class FourViewController: UIViewController {
         sharedController.fadeKeysIn(for: keysButton)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "Tap Square", viewController: self, button: self.topRightButton, gesture: nil, view: nil, segue: "openingSegue")
+            self.sharedController.addRiddleAlert(riddle: "", answer: "music", clue: "Tap Square", viewController: self, button: self.topRightButton, gesture: nil, view: nil, segue: "openingSegue")
         })
     }
     
@@ -78,7 +78,7 @@ class FourViewController: UIViewController {
         sharedController.rotateKeysLeft(for: keysButton)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "Hold for Three", viewController: self, button: nil, gesture: self.bottomLeftHoldRecognizer, view: self.bottomLeftHoldView, segue: "openingSegue")
+            self.sharedController.addRiddleAlert(riddle: "", answer: "wind", clue: "Hold for Three", viewController: self, button: nil, gesture: self.bottomLeftHoldRecognizer, view: self.bottomLeftHoldView, segue: "openingSegue")
         })
     }
     
@@ -87,7 +87,7 @@ class FourViewController: UIViewController {
         sharedController.rotateKeysDown(for: keysButton)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "Hold for Three", viewController: self, button: nil, gesture: self.bottomRightHoldRecognizer, view: self.bottomRightHoldView, segue: "openingSegue")
+            self.sharedController.addRiddleAlert(riddle: "", answer: "beach", clue: "Hold for Three", viewController: self, button: nil, gesture: self.bottomRightHoldRecognizer, view: self.bottomRightHoldView, segue: "openingSegue")
         })
     }
     
@@ -96,7 +96,7 @@ class FourViewController: UIViewController {
         sharedController.rotateKeysRight(for: keysButton)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.sharedController.addRiddleAlert(riddle: "", answer: "", clue: "Swipe Right", viewController: self, button: nil, gesture: self.swipeRightRecognizer, view: self.swipeView, segue: "openingSegue")
+            self.sharedController.addRiddleAlert(riddle: "", answer: "flight", clue: "Swipe Right", viewController: self, button: nil, gesture: self.swipeRightRecognizer, view: self.swipeView, segue: "openingSegue")
         })
     }
     
@@ -106,7 +106,7 @@ class FourViewController: UIViewController {
         
         UserDefaults.standard.set(true, forKey: "isOn5")
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.sharedController.addTransitionRiddleAlert(riddle: "", answer: "", viewController: self, segue: "5Segue", level: "4", homeSegue: "openingSegue")
+            self.sharedController.addTransitionRiddleAlert(riddle: "", answer: "ocean", viewController: self, segue: "5Segue", level: "4", homeSegue: "openingSegue")
         })
     }
     
